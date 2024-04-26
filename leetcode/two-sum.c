b2f65d26b70692b52f45184ebf578892517cc57c
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-04-26 16:10:53 +0530

    two sum: HashMap

--- leetcode/two-sum.c
+++ leetcode/two-sum.c
@@ -2,6 +2,8 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
 
     //going through the array in a double for loop and checking if sum of 2 elements equals the target. we return the index in a array alongside the size of the same
 
+    //Time: O(n^2)
+
     int n = numsSize;
     
     int* arr;
@@ -24,4 +26,27 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     *returnSize=0;
     return 0;
 
+}
+
+
+//using rust. Time:O(n)
+
+//goes through each number in the list and calculates what other number we need to reach the target by subtracting the current number from the target. If we've already seen this needed number before, it means we've found the pair that adds up to the target, so it returns their positions. Otherwise, it stores the current number in the HashMap for future reference.
+
+use std::collections::HashMap;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+
+        for (index, current_element) in nums.iter().enumerate(){
+            let needed = target - current_element;
+
+            if map.contains_key(&needed){
+                return vec![map[&needed], index as i32];
+            }
+            map.insert(current_element, index as i32);
+
+        }
+        return Vec::new();
+    }
 }
